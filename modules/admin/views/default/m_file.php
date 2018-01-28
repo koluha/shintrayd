54a8e2eebe92c73b69d46b5c051fa71d38394449
Koluha <kolyan2288@mail.ru>
2018-01-28 22:13:06 +0300

    1111

--- modules/admin/views/default/m_file.php
+++ modules/admin/views/default/m_file.php
@@ -1,4 +1,5 @@
 <?php
+
 use mihaildev\elfinder\InputFile;
 use mihaildev\elfinder\ElFinder;
 use yii\web\JsExpression;
@@ -7,11 +8,13 @@ mihaildev\elfinder\Assets::noConflict($this);
 ?>
 <div class="admin-default-index">
     <h2>Менеджер файлов</h2>
+    <p><b>db_disk:</b> содержит картинки базы дисков</p>
+    <p><b>db_tire:</b> содержит картинки базы шин</p>
     <?php
     echo ElFinder::widget([
         'language' => 'ru',
         'controller' => 'elfinder', // вставляем название контроллера, по умолчанию равен elfinder
-     //   'filter' => 'image', // фильтр файлов, можно задать массив фильтров https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#wiki-onlyMimes
+        //   'filter' => 'image', // фильтр файлов, можно задать массив фильтров https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#wiki-onlyMimes
         'callbackFunction' => new JsExpression('function(file, id){}') // id - id виджета
     ]);
     ?>
--- modules/admin/views/layouts/admin-main.php
+++ modules/admin/views/layouts/admin-main.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
 AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
-<!DOCTYPE html>
+
 <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
