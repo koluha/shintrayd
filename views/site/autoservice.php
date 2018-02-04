2891c0fbe8e3ff6a8fde10ef257cc504b84e231e
Koluha <kolyan2288@mail.ru>
2018-02-04 16:00:55 +0300

    log1

--- views/site/autoservice.php
+++ views/site/autoservice.php
@@ -40,16 +40,50 @@ $this->registerMetaTag([
             <p>
                 Каждый клиент может лично наблюдать за ремонтом автомобиля, чтобы удостовериться в обнаруженных неисправностях, а так же убедиться в квалификации наших специалистов. Вы всегда будете знать, кто из работников центра отвечает за авто и кто выполняет ремонт.
             </p>
-            
-            <ul>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Диагностика автомобиля</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Ремонт как бензиновых, так и дизельных двигателей</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Ремонт и обслуживание трансмиссии</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Ремонт и обслуживание тормозной системы</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Промывка инжектора</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Заправка и ремонт автокондиционеров</a></li>
-                <li><a href="<?php echo Url::toRoute('site/'); ?>">Ремонт дисков</a></li>
-             </ul>
+
+            <ul class="autoservice">
+                <li>
+                    <span>Сварка Аргон(TIG\MIG)</span>
+                    <?= Html::a(Html::img("/img/page/Svarka_Argon.jpg", ['class' => 'img_au', 'alt' => 'Сварка Аргон(TIG\MIG)', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Тонировка и бронепленка</span>
+                    <?= Html::a(Html::img("/img/page/Tonirovka_i_broneplenka.jpg", ['alt' => 'Тонировка и бронепленка', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Автозапчасти (в наличии и под заказ)</span>
+                    <?= Html::a(Html::img("/img/page/Avtozapchasti_v_nalichii_i_pod_zakaz.jpg", ['alt' => 'Автозапчасти (в наличии и под заказ)', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Компьютерная диагностика автомобиля</span>
+                    <?= Html::a(Html::img("/img/page/Kompyuternaya_diagnostika_avtomobilya.jpg", ['alt' => 'Компьютерная диагностика автомобиля', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Ремонт как бензиновых, так и дизельных двигателей</span>
+                    <?= Html::a(Html::img("/img/page/Remont_kak_benzinovyh_tak_i_dizelnyh_dvigatelej.jpg", ['alt' => 'Ремонт как бензиновых, так и дизельных двигателей', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Ремонт и обслуживание трансмиссии</span>
+                    <?= Html::a(Html::img("/img/page/Remont_i_obsluzhivanie_transmissii.jpeg", ['alt' => 'Ремонт и обслуживание трансмиссии', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Ремонт и обслуживание тормозной системы</span>
+                    <?= Html::a(Html::img("/img/page/Remont_i_obsluzhivanie_tormoznoj_sistemy.jpg", ['alt' => 'Ремонт и обслуживание тормозной системы', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Промывка инжектора</span>
+                    <?= Html::a(Html::img("/img/page/Promyvka_inzhektora.jpg", ['alt' => 'Промывка инжектора', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Заправка и ремонт автокондиционеров</span>
+                    <?= Html::a(Html::img("/img/page/Zapravka_i_remont_avtokondicionerov.jpg", ['alt' => 'Заправка и ремонт автокондиционеров', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+                <li>
+                    <span>Ремонт дисков</span>
+                    <?= Html::a(Html::img("/img/page/Remont_diskov.jpg", ['alt' => 'Ремонт дисков', 'width' => '320', 'height' => '200']), ['site/']) ?>
+                </li>
+            </ul>
         </div>
     </div>
 </div>
+
